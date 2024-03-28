dfd0053577259a587112200b8697b6947abff763
webanilkumar <gawolanil@gmail.com>
2024-03-28 17:20:09 +0530

    Add project files.

---
+++ TestApp2/Program.cs
@@ -0,0 +1,208 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Net;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        //if_ else Statement (1)
+
+        //Console.WriteLine("Enter the number");
+        //int i = Convert.ToInt32(Console.ReadLine());
+        //if(i%2 == 0)
+        //{
+        //    Console.WriteLine("It is Even Number");
+        //}
+        //else
+        //{
+        //    Console.WriteLine("It is Odd Number");
+        //}
+        //Console.ReadLine();
+
+        //If_ Else, Else_IF Statement (2)
+
+        //Console.WriteLine("Enter the student Marks");
+        //int i = Convert.ToInt32(Console.ReadLine());
+        //if (i>=75)
+        //{
+        //    Console.WriteLine("A Grade");
+        //}
+        //else if(i>60 && i<75)
+        //{
+        //    Console.WriteLine("B Grade");
+        //}
+        //else
+        //{
+        //    Console.WriteLine("C Grade");
+        //}
+        //Console.ReadLine();
+
+        //switch Case
+
+        //Console.WriteLine("Enter the first number");
+        //int i = Convert.ToInt32(Console.ReadLine());
+        //Console.WriteLine("Enter the second number");
+        //int j = Convert.ToInt32(Console.ReadLine());
+        //int result = 0;
+        //Console.WriteLine("Enter the input number +,-,/,*,%");
+        //char ad = Convert.ToChar(Console.ReadLine());
+        //switch(ad)
+        //{
+        //    case '+': result = i + j;
+        //        break;
+        //    case '-':
+        //        result = i - j;
+        //        break;
+        //    case '/':
+        //        result = i / j;
+        //        break;
+        //    case '*':
+        //        result = i / j;
+        //        break;
+        //    case '%':
+        //        result = i % j;
+        //        break;
+        //    default: Console.WriteLine("Wrong Input");
+        //        break;
+
+        //}
+        //Console.WriteLine("Result is :" + result);
+        //Console.ReadLine();
+
+        //For Loop
+
+        //for (int i=2; i<=20; i=i+2)
+        //{
+        //    Console.WriteLine(i);
+        //}
+        //Console.ReadLine();
+
+        //Nasted Loops
+
+        //Console.WriteLine("i   j");
+        //for (int i=1;i<=3;i++)
+        //{
+        //    for (int j=1;j<=5;j++)
+        //    {
+        //        Console.WriteLine(i + "   " + j);
+        //    }              
+        //}
+        //Console.ReadLine();
+
+        //While Loop
+
+        //int i = 1;
+        //while(i<=20)
+        //{
+        //    Console.WriteLine(i);
+        //    i = i + 2;
+        //}
+        //Console.ReadLine();
+
+        //Nasted While Loop
+
+        //int i = 1;
+        //Console.WriteLine("i   j");
+        //while(i<=2)
+        //{
+        //    int j = 1;
+        //    while(j<=3)
+        //    {
+        //        Console.WriteLine(i + "  "+ j);
+        //        j++;
+        //    }
+        //    i++;
+        //}
+        //Console.ReadLine();
+
+        //do while Loop
+
+        //int i = 24;
+        //do
+        //{
+        //    Console.WriteLine(i);
+        //    i = i + 2;     
+        //} while (i <= 20);
+        //Console.ReadLine();
+        //-------------------------------//
+
+        //Break And Continue Statement
+        //Break Statement
+
+        //for(int i = 1; i<=10; i++)
+        //{
+        //    Console.WriteLine(i);
+        //    if (i == 5)
+        //        break;
+        //}
+        //Console.WriteLine("After the Loop");
+        //Console.ReadLine();
+
+        //Nasted Break Statement
+        //for (int i = 1; i<=10; i++)
+        //{
+        //    for(int j = 1; j<=5;j++)
+        //    {
+        //        if (j == 3)
+        //            break;
+        //        Console.WriteLine(i + " " + j);
+        //    }
+        //}
+        //Console.WriteLine("After the Loop");
+        //Console.ReadLine();
+
+        // Continue Loop
+
+        //for (int i = 1; i <=10; i++)
+        //{
+        //    if (i == 4)
+        //        continue;
+        //    Console.WriteLine(i);
+        //}
+        //Console.WriteLine("After the Loop");
+        //Console.ReadLine();
+
+        // Continue Nasted Loop Statement
+
+        //for (int i = 1; i<=4; i++)
+        //{
+        //    for(int j = 1; j<=4; j++)
+        //    {
+        //        if(i==2 && j==2)
+        //        {
+        //            continue;
+        //        }
+        //        Console.WriteLine(i + "  " + j);
+        //    }
+        //}
+        //Console.WriteLine("After the Loop");
+        //Console.ReadLine();
+
+            //GOTO Statement
+        //l1:
+        //    Console.WriteLine("Number is less then 10");
+        //    Console.WriteLine("Enter the number");
+        //    int i = Convert.ToInt32(Console.ReadLine());
+        //    if(i<10)
+        //    {
+        //        goto l1;
+        //    }
+        //    else
+        //    {
+        //        Console.WriteLine("Number is Greater than 10");
+        //    }
+        //    Console.WriteLine();
+
+            //Function in C Sharp
+
+        }
+    }
+}
